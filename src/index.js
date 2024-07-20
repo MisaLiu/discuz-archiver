4aedbb2ddb18090f90586a8ee104ce3a05230cd8
Misa Liu <misaliu@misaliu.top>
2024-07-20 12:40:26 +0800

    feat: Generate field page

--- src/index.js
+++ src/index.js
@@ -13,11 +13,32 @@ const DIST_DIR = path.resolve(__rootdir, './dist');
 const DISCUZ_DATA_DIR = path.resolve(__rootdir, './data');
 
 
+(async () => {
+
+// Create dist dir if not exist
 if (!fs.existsSync(DIST_DIR)) {
   fs.mkdirSync(DIST_DIR);
 }
 
-Discuz.getAllFields()
-  .then(e => View.parseFile('home.tmpl', path.resolve(DIST_DIR, './index.html'), { fields: e }));
+// Generate index.html
+const Fields = await Discuz.getAllFields();
+await View.parseFile('home.tmpl', path.resolve(DIST_DIR, './index.html'), { fields: Fields })
 
+// Create field dist dir if not exist
+if (!fs.existsSync(path.resolve(DIST_DIR, './f'))) {
+  fs.mkdirSync(path.resolve(DIST_DIR, './f'));
+}
+
+// Generate /f/*.html
+for (const field of Fields) {
+  for (const sub of field.subfields) {
+      const Threads = await Discuz.getFieldThreads(sub.fid);
+    await View.parseFile('field.tmpl', path.resolve(DIST_DIR, `./f/${sub.fid}.html`), { field: sub, threads: Threads });
+  }
+}
+
+// Disconnect database
+await Discuz.disconnect();
+
+})();
 
